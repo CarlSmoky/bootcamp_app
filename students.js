88958eda0eb56ee90438602e8495d7ba566e926a
tsumitakaoru <k.tsumita01@gmail.com>
2021-11-24 20:49:50 -0500

    Parameterize queries

--- students.js
+++ students.js
@@ -14,13 +14,20 @@ pool.connect().then(() => {
   console.log(e.message);
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then(res => {
     // console.log(res);
     // console.log(res.rows);
